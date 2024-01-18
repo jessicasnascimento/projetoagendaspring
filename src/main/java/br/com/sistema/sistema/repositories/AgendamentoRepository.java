fa3749487f92fe1b6bb07fa9689d252debfe67c5
Jessica Sousa <jessicamedisse@gmail.com>
2024-01-18 07:09:38 -0300

    Add o método findByData no JpaRespository

--- src/main/java/br/com/sistema/sistema/repositories/AgendamentoRepository.java
+++ src/main/java/br/com/sistema/sistema/repositories/AgendamentoRepository.java
@@ -4,7 +4,10 @@ import br.com.sistema.sistema.entities.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Integer> {
-
+    List<Agendamento> findByData(LocalDate data);
 }
