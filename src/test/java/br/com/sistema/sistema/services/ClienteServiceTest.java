1bd9317e5293cad228490947e212eb76f9007848
Jessica Nascimento <jessicamedisse@gmail.com>
2024-02-01 11:47:35 -0300

    Adicionando teste unitario JUnit para inclusao de cliente

--- src/test/java/br/com/sistema/sistema/services/ClienteServiceTest.java
+++ src/test/java/br/com/sistema/sistema/services/ClienteServiceTest.java
@@ -3,23 +3,37 @@ package br.com.sistema.sistema.services;
 
 import br.com.sistema.sistema.dtos.ClienteDTO;
 import br.com.sistema.sistema.utils.DataUtil;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
 
+@SpringBootTest
 public class ClienteServiceTest {
 
     @Autowired
     private ClienteService clienteService;
 
-
-    public void testIncluir() {
-        ClienteDTO novoClienteDto = new ClienteDTO();
-        novoClienteDto.setNome("Jessica");
-        novoClienteDto.setDataNascimento(DataUtil.stringToLocalDate("31/08/1996"));
-        novoClienteDto.setEmail("jessica@gmail.com");
-        novoClienteDto.setProfissao("Programadora");
-
-        ClienteDTO clienteIncluidoDto = clienteService.incluir(novoClienteDto);
+    public ClienteDTO mockCliente() {
+        ClienteDTO clienteDTO = new ClienteDTO();
+        clienteDTO.setNome("Jessica");
+        clienteDTO.setDataNascimento(DataUtil.stringToLocalDate("31/08/1996"));
+        clienteDTO.setEmail("jessica@gmail.com");
+        clienteDTO.setProfissao("Programadora");
+        clienteDTO.setCpf("999998888");
+        return clienteDTO;
     }
 
+    @Test
+    public void incluirTest() {
+        ClienteDTO novoClienteDto = mockCliente();
+
+        ClienteDTO clienteIncluidoDto = clienteService.incluir(novoClienteDto);
+        assertNotNull("O cliente é não pode ser nulo", clienteIncluidoDto);
+        assertEquals(novoClienteDto.getNome(),  clienteIncluidoDto.getNome());
+        assertEquals(novoClienteDto.getCpf(), clienteIncluidoDto.getCpf());
+        assertEquals(novoClienteDto.getProfissao(), clienteIncluidoDto.getProfissao());
+    }
 }
